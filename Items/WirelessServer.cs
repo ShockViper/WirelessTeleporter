f35a981a4f0ff4a880ec4bd9d3b399108a996fa5
ShockViper <shockviper@gmail.com>
2018-10-23 21:52:07 +0200

    added server item and tile

---
+++ Items/WirelessServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace WirelessTeleporter.Items
+{
+    class WirelessServer : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Wireless Server");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 26;
+            item.height = 39;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.rare = 1;
+            item.maxStack = 1;
+            item.value = Item.sellPrice(0, 50);
+            item.createTile = mod.TileType("WirelessServer");
+            // Set other item.X values here
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.IronBar,10);
+            //recipe.AddIngredient(ItemID.Teleporter);
+            recipe.AddTile(TileID.TinkerersWorkbench);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/WirelessServer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+
+namespace WirelessTeleporter.Tiles
+{
+    class WirelessServer : ModTile
+    {
+        public override void SetDefaults()
+        {
+            
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
+            TileObjectData.newTile.Origin = new Point16(0, 3);
+            //TileObjectData.newTile.AnchorBottom = new Terraria.DataStructures.AnchorData(Terraria.Enums.AnchorType.SolidTile, 3, 0);
+            //TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.addTile(Type);
+            //dustType = mod.DustType("Sparkle");
+            //drop = mod.ItemType("WirelessTeleporterBase");
+            AddMapEntry(new Color(200, 200, 200));
+            // Set other values here
+        }
+
+ 
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            // the 3rd and 4th numbers should be 16*width blocks and 16 * height blocks respectively.
+            Item.NewItem(i * 16, j * 16, 48, 64, mod.ItemType("WirelessTeleporterBase"));
+        }
+    }
+}
