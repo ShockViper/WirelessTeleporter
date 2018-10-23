9cf9a8c4ecd7b1bcd3ddaa59f72d1a6b5d246341
ShockViper <shockviper@gmail.com>
2018-10-23 17:05:46 +0200

    Add project files.

---
+++ Items/WirelessTeleporterBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace WirelessTeleporter.Items
+{
+    class WirelessTeleporterBase : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Wireless Teleporter Base");
+        }
+
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.Teleporter);
+            item.color = Color.Blue;
+            item.value = Item.sellPrice(0, 25);
+            item.createTile = mod.TileType("WirelessTeleporterBase");
+            // Set other item.X values here
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.IronBar,10);
+            recipe.AddIngredient(ItemID.Teleporter);
+            recipe.AddTile(TileID.TinkerersWorkbench);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Tiles/WirelessTeleporterBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+
+namespace WirelessTeleporter.Tiles
+{
+    class WirelessTeleporterBase : ModTile
+    {
+        public override void SetDefaults()
+        {
+            
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+            TileObjectData.newTile.Height = 1;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16};
+            TileObjectData.newTile.AnchorBottom = new Terraria.DataStructures.AnchorData(Terraria.Enums.AnchorType.SolidTile, 3, 0);
+            TileObjectData.newTile.UsesCustomCanPlace = true;
+            TileObjectData.addTile(Type);
+            //dustType = mod.DustType("Sparkle");
+            drop = mod.ItemType("WirelessTeleporterBase");
+            AddMapEntry(new Color(200, 200, 200));
+            // Set other values here
+        }
+    }
+}
