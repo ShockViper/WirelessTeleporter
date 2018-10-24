96c3c8c9d34032c1ade7eaa4eac2ec118d04504f
ShockViper <shockviper@gmail.com>
2018-10-24 22:18:33 +0200

    test tooltip

--- WirelessTeleporter.cs
+++ WirelessTeleporter.cs
@@ -4,10 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Terraria.ModLoader;
+using WirelessTeleporter.Tiles;
 
 namespace WirelessTeleporter
 {
     public class WirelesTeleporter : Mod
     {
+        internal static WirelesTeleporter instance;
+        internal static  Dictionary<string,int> servers;
+
+        public override void Load()
+        {
+            instance = this;
+            servers = new Dictionary<string, int>;
+
+        }
+        public override void Unload()
+        {
+            instance = null;
+            servers = null;
+
+        }
     }
+
 }
